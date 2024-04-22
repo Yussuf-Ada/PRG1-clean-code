c69ee76dc88907f2dcd0fffe938ca087a6bae7fd
Yussuf-Ada <167077054+Yussuf-Ada@users.noreply.github.com>
2024-04-22 10:43:16 +0000

    Refactor functions

--- program.js
+++ program.js
@@ -1,34 +1,33 @@
-let tList = []; 
+let tasksList = []; 
 
-function x1(tName, tDue) {
-  let t = { n: tName, d: tDue, c: false };
-  tList.push(t);
+function addingNewTasks(taskName, taskDue) {
+  let task = { name: taskName, due: taskDue, completed: false };
+  tasksList.push(task);
 }
 
-function x2(tName) {
-  tList = tList.filter(t => t.n !== tName);
+function searchingForTask(taskName) {
+  tasksList = tasksList.filter(taskName => taskName.name !== taskName);
 }
 
-function x3() {
+function displayAllTask() {
   console.log("All Tasks:");
-  tList.forEach(t => {
-    console.log(`Task: ${t.n}, Due: ${t.d}, Completed: ${t.c}`);
+  tasksList.forEach(task => {
+    console.log(`Task: ${task.name}, Due: ${task.due}, Completed: ${task.completed}`);
   });
 }
 
-function x4(tName) {
-  let tIndex = tList.findIndex(t => t.n === tName);
-  if (tIndex !== -1) {
-    tList[tIndex].c = true;
+function completeTask(tName) {
+  let taskIndex = tasksList.findIndex(task => task.name === tName);
+  if (taskIndex !== -1) {
+    tasksList[taskIndex].completed = true;
   } else {
     console.log("Task not found.");
   }
 }
 
 
-x1("Fix bug in code", "2024-02-21");
-x1("Update documentation", "2024-02-22");
-x3();
-x4("Fix bug in code");
-x2("Update documentation");
-x3();
+addingNewTasks("Fix bug in code", "2024-02-21");
+addingNewTasks("Update documentation", "2024-02-22");
+displayAllTask();
+searchingForTask("Fix bug in code");
+completeTask("Update documentation");
